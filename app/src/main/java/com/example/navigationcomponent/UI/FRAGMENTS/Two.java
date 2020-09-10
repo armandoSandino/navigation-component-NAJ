a59d4721165990e3ad4fd57cd2740ea7613acb89
ArmandoS <jsandino@aimservices.tech>
2020-09-10 08:30:36 +0200

    chore; add two fragment

--- app/src/main/java/com/example/navigationcomponent/UI/FRAGMENTS/Two.java
+++ app/src/main/java/com/example/navigationcomponent/UI/FRAGMENTS/Two.java
@@ -2,59 +2,24 @@ package com.example.navigationcomponent.UI.FRAGMENTS;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.navigationcomponent.R;
+import com.google.android.material.button.MaterialButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Two#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Two extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Two() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Two.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Two newInstance(String param1, String param2) {
-        Two fragment = new Two();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +27,30 @@ public class Two extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MaterialButton btnAtras =  view.findViewById(R.id.btnAtras);
+        MaterialButton btnSiguiente = view.findViewById(R.id.btnSiguiente);
+
+        final NavController navController = Navigation.findNavController(  view );
+
+        btnAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // llamamos al fragmento
+                Navigation.findNavController( v ).navigate( R.id.inicio );
+            }
+        });
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Navigation.findNavController( v ).navigate( R.id.fragmentTwo );
+                navController.navigate(R.id.three);
+            }
+        });
     }
 }
